b5abd3315f650ff0f5020fa0f08a8958c45f7302
isaacn123 <nsambai72@gmail.com>
2022-05-15 14:56:46 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ImagePicker } from '@awesome-cordova-plugins/image-picker/ngx';
+import { Camera } from '@awesome-cordova-plugins/camera/ngx';
+import { File } from '@awesome-cordova-plugins/file/ngx';
+import { Crop } from '@ionic-native/crop/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  File,
+  Camera,
+  ImagePicker,
+  Crop,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,12 @@
+/* eslint-disable @typescript-eslint/prefer-for-of */
+/* eslint-disable object-shorthand */
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+import { ImagePicker, ImagePickerOptions } from '@awesome-cordova-plugins/image-picker/ngx';
+import { Crop } from '@ionic-native/crop/ngx';
+import { ActionSheetController } from '@ionic/angular';
+import { File } from '@awesome-cordova-plugins/file/ngx';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +14,79 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  imageurl: any;
+  securepath: any = window;
+  url: any;
 
-  constructor() {}
+  constructor(private actionsheet: ActionSheetController,
+    private camera: Camera, private file: File,
+    private imagepicker: ImagePicker, private crop: Crop, private domsanitize: DomSanitizer) {}
+
+
+  chooseFromCamera(sourceType){
+    const options: CameraOptions = {
+       quality: 100,
+       mediaType: this.camera.MediaType.PICTURE,
+       destinationType: this.camera.DestinationType.FILE_URI,
+       encodingType: this.camera.EncodingType.JPEG,
+       sourceType: sourceType,
+    };
+
+    this.camera.getPicture(options).then((result) => {
+      console.log('Camera URL',result);
+      // this.imageurl = result;
+      this.imageurl = this.securepath.Ionic.WebView.convertFileSrc(result);
+    }, error=>{
+      console.log('Error CAMERA', error);
+    });
+  }
+
+  santizeUrl(imageUrl){
+    return this.domsanitize.bypassSecurityTrustUrl(imageUrl);
+  }
+
+  pickImagesFromLibrary(){
+    const options: ImagePickerOptions = {
+      quality: 100,
+      maximumImagesCount: 1,
+    };
+    this.imagepicker.getPictures(options).then((imageresult)=> {
+    console.log('image Picker Results', imageresult);
+
+     for(let i=0; i<imageresult.length; i++){
+      this.url = this.securepath.Ionic.WebView.convertFileSrc(imageresult[i]);
+     }
+    }, rror=>{
+      console.log('Image Picker Error:', rror);
+    });
+  }
+
+ async selectimageOptions(){
+    const actionsheet = await this.actionsheet.create({
+     header: 'Select image Source',
+     buttons: [
+       {
+         text: 'Load from Gallery',
+         handler: ()=>{
+           this.pickImagesFromLibrary();
+           console.log('Image Selected from Gallery');
+         }
+       },
+       {
+         text: 'Select Camera',
+         handler: ()=>{
+           this.chooseFromCamera(this.camera.PictureSourceType.CAMERA);
+           console.log('Camera Selected');
+         }
+       },
+       {
+         text: 'Cancel',
+         role: 'cancel'
+       }
+     ]
+    });
+    await actionsheet.present();
+  }
+
 
 }
